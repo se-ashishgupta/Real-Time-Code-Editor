5b2be91f21941383ce5ec7facec1c321b837911f
Ashish Gupta <gashish4950@gmail.com>
2023-09-07 17:25:12 +0530

    Fisrt commit

--- client/src/components/Client.jsx
+++ client/src/components/Client.jsx
@@ -4,7 +4,7 @@ import Avatar from "react-avatar";
 const Client = ({ userName }) => {
   return (
     <div className=" flex items-center gap-2 ">
-      <Avatar name={userName} size="50px" round="40px" />
+      <Avatar name={userName} size={50} round={50} />
       <span className=" text-sm text-white ">{userName}</span>
     </div>
   );
